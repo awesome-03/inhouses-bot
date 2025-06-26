0f756ddea8fe9109850ae2ced7f8e043ce06569c
awesome-03 <awesomedev03@gmail.com>
2025-06-26 19:01:16 +0300

    change database to postgres

--- database/connect.py
+++ database/connect.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
+DB_USER = os.getenv("DB_USER", "your_db_username")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-engine = create_engine("sqlite:///database/database.db", echo=True)
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
