70ecbcdd6124e9176e57f8131ab9c47e67a3fa91
awesome-03 <awesomedev03@gmail.com>
2025-07-01 20:36:10 +0300

    migrate to postgresdb

--- database/connect.py
+++ database/connect.py
@@ -1,6 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-engine = create_engine("sqlite:///database/database.db", echo=True)
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
